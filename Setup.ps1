016a106b744c0ee1ef94b2d6ebe49ec99d0e6d1b
Zach Luety <zluety@gpwa.com>
2021-09-01 13:32:53 -0700

    Add windows setup

---
+++ Setup.ps1
@@ -0,0 +1,19 @@
+#Requires -RunAsAdministrator
+
+Param(    
+    [switch]
+    $IncludeSuggested
+)
+
+# 1. Download Chocolatey (https://chocolatey.org/)
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+$Manifest = "r.project", "python", "git" 
+
+if ($IncludeSuggested) {
+    # Add suggested packages to the manifest
+    $OptionalPackages = "r.studio", "vscode", "gh"
+    $Manifest += $OptionalPackages
+}
+
+choco install -y $Manifest
