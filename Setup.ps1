b227cfb5ac7ca8f1522ae7feeeead7ce230fdb19
Zach Luety <zluety@gpwa.com>
2021-09-07 09:59:02 -0700

    updated python package from python to anaconda3

--- Setup.ps1
+++ Setup.ps1
@@ -8,7 +8,7 @@ Param(
 # 1. Download Chocolatey (https://chocolatey.org/)
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$Manifest = "r.project", "rtools", "python", "git" 
+$Manifest = "r.project", "rtools", "anaconda3", "git" 
 
 if ($IncludeSuggested) {
     # Add suggested packages to the manifest
