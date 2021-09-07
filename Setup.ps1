f6bae2fc8a1a8ee7c4d4f577d1215d2c5b3eac92
Zachary Luety <89766428+zrluety@users.noreply.github.com>
2021-09-07 09:31:35 -0700

    Add RTools to manifest

--- Setup.ps1
+++ Setup.ps1
@@ -8,7 +8,7 @@ Param(
 # 1. Download Chocolatey (https://chocolatey.org/)
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$Manifest = "r.project", "python", "git" 
+$Manifest = "r.project", "rtools", "python", "git" 
 
 if ($IncludeSuggested) {
     # Add suggested packages to the manifest
