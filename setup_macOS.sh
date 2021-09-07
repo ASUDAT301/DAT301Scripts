496d9f0c42f5e73658946df75105d12c52b3577a
Zach Luety <zluety@gpwa.com>
2021-09-07 12:16:11 -0700

    Create bash script to install homebrew and required packages on macOS

---
+++ setup_macOS.sh
@@ -0,0 +1,15 @@
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install r
+brew install --cask r
+brew install --cask rstudio
+
+
+# install python and ide
+brew install --cask anaconda
+brew install --cask visual-studio-code
+
+# install git
+brew install --cask git
+brew install gh
