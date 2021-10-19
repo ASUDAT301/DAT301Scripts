b4ec813317872242139a1aad31d4d890f031dde3
Zachary Luety <89766428+zrluety@users.noreply.github.com>
2021-10-19 10:17:57 -0700

    Update setup_windows.ps1
    Remove optional flag

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -1,19 +1,8 @@
 #Requires -RunAsAdministrator
 
-Param(    
-    [switch]
-    $IncludeSuggested
-)
-
 # 1. Download Chocolatey (https://chocolatey.org/)
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$Manifest = "r.project", "rtools", "anaconda3", "git", "miktex"
-
-if ($IncludeSuggested) {
-    # Add suggested packages to the manifest
-    $OptionalPackages = "r.studio", "vscode", "gh"
-    $Manifest += $OptionalPackages
-}
+$Manifest = "r.project", "rtools", "anaconda3", "git", "miktex", "r.studio", "vscode", "gh"
 
 choco install -y $Manifest
